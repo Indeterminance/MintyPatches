52c6c1a961e6cbc5b20b0c791d7466164c75eb8c
Indeterminance <75706610+Indeterminance@users.noreply.github.com>
2022-02-15 00:43:22 +0000

    Update history patch info

--- practice_set_history/patch.js
+++ practice_set_history/patch.js
@@ -1,4 +1,7 @@
 {
-	"id": "history_plus",
-	"title": "Implements the MASTER feature for spell histories in games before TD, and additionally displays the history of consecutive attempts below the spell history."
+	"id"; "practice_set_history",
+	"title"; "Adds a set feature to keep track of spell captures in a session. Also implements some minor cosmetic changes to spell histories, such as the modern era's MASTER feature. Early debug version, currently only does anything meaningful in SA.",
+	"dependencies"; [
+		"nmlgc/base_tsa"
+	]
 }

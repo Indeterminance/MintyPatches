86be8a27fed19121c626755619a24152c969bdea
Indeterminance <75706610+Indeterminance@users.noreply.github.com>
2023-05-17 22:20:18 +0100

    Prepare for thcrap publishing
    - Add unfinished history patch to gitignore
    - Release Blemishes, since it's funny

--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
 	"contact": "Indeterminance#3373",
 	"id": "mintymods",
 	"patches": {
-		"resource_limiter": "Patch that adds an option for max extends, preventing you from getting any extra lives after you've reached your limit. Currently only supports LoLK."
+		"resource_limiter": "Patch that adds an option for max extends, preventing you from getting any extra lives after you've reached your limit. Currently only supports LoLK.",
+		"blemishes": "A patch that solely changes Overwhelming Blemishes on Hard difficulty, since it clearly isn't overwhelming enough. "
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/Indeterminance/MintyPatches/main/"

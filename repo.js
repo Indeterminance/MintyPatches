86ba9e43345bca54d15a6d06aa4fb0bab7a7cff6
Indeterminance <75706610+Indeterminance@users.noreply.github.com>
2023-05-17 22:12:34 +0100

    Create repo.js

---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "Indeterminance#3373",
+	"id": "mintymods",
+	"patches": {
+		"resource_limiter": "Patch that adds an option for max extends, preventing you from getting any extra lives after you've reached your limit. Currently only supports LoLK."
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/Indeterminance/MintyPatches/main/"
+	],
+	"title": "Mints' Semi-Functional Patch Repository"
+}

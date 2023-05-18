55761252773507be3b2a6c80c6652a197a76e8f6
Indeterminance <75706610+Indeterminance@users.noreply.github.com>
2023-05-18 01:00:50 +0100

    Quick typo fixes

--- blemishes/patch.js
+++ blemishes/patch.js
@@ -3,6 +3,6 @@
   "title": "A small change for Overflowing Blemishes to adjust the difficulty more in line with \"Hard\".",
   "servers": [
       "https://raw.githubusercontent.com/Indeterminance/MintyPatches/main/blemishes",
-		"https://mirrors.thpatch.net/MintyPatches/blemishes/"
+      "https://mirrors.thpatch.net/MintyPatches/blemishes/"
   ]
 }
--- resource_limiter/patch.js
+++ resource_limiter/patch.js
@@ -3,9 +3,9 @@
 	"title": "A patch to help restrict resource usage.",
 	"dependencies": [
 		"nmlgc/base_tsa"
-	]
+	],
 	"servers": [
 		"https://raw.githubusercontent.com/Indeterminance/MintyPatches/main/resource_limiter/",
 		"https://mirrors.thpatch.net/MintyPatches/resource_limiter/"
-	],
+	]
 }

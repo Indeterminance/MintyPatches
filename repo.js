4ecd09823701a0af3cc05f082d427fc25bb231bc
Indeterminance <75706610+Indeterminance@users.noreply.github.com>
2023-05-21 00:39:38 +0100

    Revert "Fixed brackets"
    This reverts commit c3e42b41d15c7fca95f0c7fa90f43f8db2da83d1.

--- repo.js
+++ repo.js
@@ -1,10 +1,10 @@
 {
 	"contact": "Indeterminance#3373",
 	"id": "mintymods",
-	"patches": [
+	"patches": {
 		"resource_limiter": "Patch that adds an option for max extends, preventing you from getting any extra lives after you've reached your limit. Currently only supports LoLK.",
 		"blemishes": "A patch that solely changes Overwhelming Blemishes on Hard difficulty, since it clearly isn't overwhelming enough. "
-	],
+	},
 	"servers": [
 		"https://mirrors.thpatch.net/MintyPatches/",
 		"https://raw.githubusercontent.com/Indeterminance/MintyPatches/main/"

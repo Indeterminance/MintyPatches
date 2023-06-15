a688c6e6ebc7f690f9629288aab1527ad77492a6
Indeterminance <75706610+Indeterminance@users.noreply.github.com>
2023-06-15 02:02:24 +0100

    Add "practice variants" patch
    Mostly just an example of a "bad" version of Clownpiece's first non right now, but planned to be a patch full of practice variations of patterns

---
+++ practice_variants/patch.js
@@ -0,0 +1,9 @@
+{
+  "contact": "Indeterminance#3373",
+  "id": "blemishes",
+  "title": "A small change for Overflowing Blemishes to adjust the difficulty more in line with \"Hard\".",
+  "servers": [
+      "https://mirrors.thpatch.net/mintymods/blemishes/",
+      "https://raw.githubusercontent.com/Indeterminance/MintyPatches/main/blemishes"
+  ]
+}

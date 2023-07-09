efe30379868b8c1f3543e1f4ccc9ff444370c631
Indeterminance <75706610+Indeterminance@users.noreply.github.com>
2023-07-09 13:09:59 +0100

    Add "pd_powerfix"

---
+++ pd_powerfix/patch.js
@@ -0,0 +1,12 @@
+{
+	"contact": "Indeterminance#3373",
+	"id": "pd_powerfix",
+	"title": "Prevents LoLK from dropping your power when dying in Pointdevice.",
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"servers": [
+		"https://mirrors.thpatch.net/mintymods/pd_powerfix/",
+		"https://raw.githubusercontent.com/Indeterminance/MintyPatches/main/pd_powerfix/"
+	]
+}
---
+++ pd_powerfix/th15.v1.00b.js
@@ -0,0 +1,9 @@
+{
+    "binhacks": {
+        "no_power_loss": {
+            "addr": "0x43DF43",
+            "code": "90 90 90",
+            "expected": "89 56 50"
+        }
+    }
+}
